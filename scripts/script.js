34aa67e83e97a3ebbba0f1d6d5c159480f77a67f
djustinandre <deleonjustinandre@gmail.com>
2017-10-01 02:19:18 +0800

    modified the layout of website

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,9 @@
-﻿function shapeShift(x) {
-    x.classList.toggle("change");
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-    if (event.target.matches('.menu_icon')) {
-
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-            for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
+﻿function openSlideMenu() {
+          document.getElementById('side-menu').style.width = '250px';
+          document.getElementById('main').style.marginLeft = '250px';
         }
-    }
-}
+
+        function closeSlideMenu() {
+          document.getElementById('side-menu').style.width = '0';
+          document.getElementById('main').style.marginLeft = '0';
+        }
